835a9cafdacf3a5bc6cfaf743853454a163a6752
Dfluan <dflaphp@gmail.com>
2017-12-28 18:12:20 -0200

    Movendo um ARQUIVO

--- rename/exemplo-01.php
+++ rename/exemplo-01.php
@@ -1,25 +1,21 @@
-<?php 
-
-$dir1 = 'folder_01';
-$dir2 = 'folder_02';
+<?php
+$dir1 = "folder_001";
+$dir2 = "folder_002";
 
 if (!is_dir($dir1)) mkdir($dir1);
 if (!is_dir($dir2)) mkdir($dir2);
 
-$fileName = "README.txt";
+$filename = "REDME.TXT";
+if (!file_exists($dir1 . DIRECTORY_SEPARATOR . $filename)){
+	$file = fopen($dir1 . DIRECTORY_SEPARATOR . $filename, "w+");
+	fwrite($file, date("d/m/y H:i:s"));
+	fclose($file);
+};
+rename(
+	$dir1 . DIRECTORY_SEPARATOR . $filename,
+	$dir2 . DIRECTORY_SEPARATOR . $filename
+);
 
-if (!file_exists($dir1 .DIRECTORY_SEPARATOR . $fileName)){
-	$file = fopen($dir1 .DIRECTORY_SEPARATOR . $fileName, 'w+');
-
-fwrite($file, date('y-m-d H:i:s'));
-
-fclose($file);
-
-} 
-
-rename(	$dir2 .DIRECTORY_SEPARATOR . $fileName, 
-		$dir1 .DIRECTORY_SEPARATOR . $fileName
-	);
-echo "Arquivo foi alterado com sucesso";
+echo "Arquivo foi removido com sucesso!!";
 
  ?>
