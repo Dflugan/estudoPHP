aa64109a5ad73aa12707e2790a345a75200fa722
Dfluan <dflaphp@gmail.com>
2017-12-28 01:15:12 -0200

    Download de arquivos via FILE_GET_CONTENTS

--- FGETS/exemplo-02.php
+++ FGETS/exemplo-02.php
@@ -2,15 +2,11 @@
 
 $filename = "dll.png";
 
-$base64 = base64_encode(file_get_contents($filename));
-
+$file = base64_encode(file_get_contents($filename));
 $fileinfo = new finfo(FILEINFO_MIME_TYPE);
-$type = $fileinfo->file($filename);
+$filetype = $fileinfo->file($filename);
 
-$href =  "data:". $type .";base64," . $base64;
- ?>
-<html>
-<a href="<?=$href?>" target="_blank">Link da imagem</a>
-<a href="<?=$href?>"> Link 2</a>
-<img src="<?=$href?>" >
-</html>
+$fileImg =  "data:" . $filetype . ";base64," . $file;
+
+?>
+ <img src="<?=$fileImg?>">
--- download/exemplo-01.php
+++ download/exemplo-01.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "https://www.facebook.com/MonicaSincerona/videos/711522232386836/";
+$url = "https://precoscarros.com.br/wp-content/uploads/2016/11/Novo-HB20-2018-05.jpeg";
 
 $contents = file_get_contents($url);//download do conteudo do arquivo
 
@@ -8,7 +8,18 @@ $parse = parse_url($url);//interpreta o arquivo
 
 $basename = basename($parse['path']); // pegar o nome do arquivo
 
-$file = fopen('teste\\'.$basename, "w+");
+$extension = strstr($basename, ".");
+$pathFile = 'teste\\'.$basename;
+$separetor = explode(".", $basename);
+$filename = $separetor[0];
+
+if (file_exists($pathFile)) {
+  $rand = rand(1,20);
+  $basenameNew = 'teste\\'. $filename . "_" . $rand . $extension;
+  $file = fopen($basenameNew, "w+");
+  fwrite($file, $contents);
+}
+$file = fopen($pathFile, "w+");
 
 fwrite($file, $contents);
 
@@ -16,4 +27,4 @@ fclose($file);
 
 ?>
 
-<img src="<?='teste\\'.$basename?>"/>
+<img src="<?='teste/'.$basename?>"/>
