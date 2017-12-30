08ff38b7e20222392d0fa85d845f847eb292a5ba
Dfluan <dflaphp@gmail.com>
2017-12-29 22:50:55 -0200

    Usando Curl - viacep

--- curl/exemplo-01.php
+++ curl/exemplo-01.php
@@ -1,18 +1,20 @@
-<?php 
-
+<?php
 $cep = "06380170";
 $url = "https://viacep.com.br/ws/$cep/json/";
 
 $ch = curl_init($url);
-
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
 
 $response = curl_exec($ch);
-
 curl_close($ch);
 
-$data = json_decode($response, true);
+$data = json_decode($response,true);
+
+// $rua = $data->logradouro;
+// echo $rua . "<br>";
+
+echo $data['logradouro'] . "<br>";
 
 print_r($data);
 
