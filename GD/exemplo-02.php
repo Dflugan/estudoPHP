4a8d9889c6d0da916267aece7d2087281fa4180a
Dfluan <dflaphp@gmail.com>
2018-01-03 23:19:10 -0200

    Gerando Thumbnail (GD)

--- GD/exemplo-02.php
+++ GD/exemplo-02.php
@@ -1,12 +1,12 @@
 <?php 
 $image = imagecreatefromjpeg("imagem/certificado.jpg");
 
-$titlteimage = imagecolorallocate($image, 0, 0, 0);
-$gray = imagecolorallocate($image, 100,100,100);
+$titlteimage = imagecolorallocate($image, 0, 0, 0);// cor preto
+$gray = imagecolorallocate($image, 100,100,100);// cor cinza claro
 
-imagestring($image, 5, 450, 150, "CERTIFICADO", $titlteimage);
-imagestring($image, 5, 440, 350, utf8_decode("Flávio Aparecido dos Santos"), $titlteimage);
-imagestring($image, 3,440, 370, "Concluido em. " . date("d-m-y H:i:s"), $titlteimage);
+imagestring($image, 5, 450, 150, "CERTIFICADO", $titlteimage);//titulo
+imagestring($image, 5, 440, 350, utf8_decode("Flávio Aparecido dos Santos"), $titlteimage);//nome do aluno
+imagestring($image, 3,440, 370, "Concluido em. " . date("d-m-y H:i:s"), $titlteimage);//data do certificado
 
 header("Content-type: image/jpeg");
 
